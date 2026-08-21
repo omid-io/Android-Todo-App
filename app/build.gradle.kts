ceb61b8306d82cf465ebce4fe6b6a1af8217173c
Omid Zaferi <omidzaferi@gmail.com>
2026-08-22 02:29:34 +0330

    Align coreKtx to 1.15.0 and targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 36
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.mytasks.ai"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 35
     versionCode = 2
     versionName = "1.0.1"
 
